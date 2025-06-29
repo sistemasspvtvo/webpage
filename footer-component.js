4b498fa18869ddab133e40a515419a0a0bc2fc7d
Angel Monroy <angelmc@outlook.com>
2025-06-28 23:13:37 -0600

    1

--- footer-component.js
+++ footer-component.js
@@ -5,7 +5,7 @@
                 <div class="footer-info">
                 <picture>
                     <source srcset="/assets/img/logo-blanco.webp" type="image/webp">
-                    <img src="/assets/img/logo-blanco.png" alt="Logo Monolito" width="40" height="40" alt="Monolito Ingeniería - Consultoría estructural, levantamientos, inspecciones y gestorías en Coyoacán, Ciudad de México">
+                    <img src="/assets/img/logo-blanco.png" alt="Logo Monolito" width="40" loading="lazy" height="40" alt="Monolito Ingeniería - Consultoría estructural, levantamientos, inspecciones y gestorías en Coyoacán, Ciudad de México">
                 </picture>
                 
                 <p class="slogan">"En Monolito Ingeniería, transformamos cálculos en soluciones seguras...<br>porque tu tranquilidad no se negocia."</p>
@@ -28,7 +28,7 @@
                 </div>
                 <p>&copy; ${new Date().getFullYear()} Todos los derechos reservados</p>
                 </div>
-                <a href="//www.dmca.com/Protection/Status.aspx?ID=9119aef2-a242-43b7-86f1-d2a5bb4a5a6c" title="DMCA.com Protection Status" class="dmca-badge"> <img src ="https://images.dmca.com/Badges/DMCA_logo-std-btn140w.png?ID=9119aef2-a242-43b7-86f1-d2a5bb4a5a6c" width="141" height="53"  alt="DMCA.com Protection Status" /></a>  <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
+                <a href="//www.dmca.com/Protection/Status.aspx?ID=9119aef2-a242-43b7-86f1-d2a5bb4a5a6c" title="DMCA.com Protection Status" class="dmca-badge"> <img src ="https://images.dmca.com/Badges/DMCA_logo-std-btn140w.png?ID=9119aef2-a242-43b7-86f1-d2a5bb4a5a6c" loading="lazy" width="141" height="53"  alt="DMCA.com Protection Status" /></a>  <script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
             </footer>
         `;
     }

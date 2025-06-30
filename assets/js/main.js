a85da615cd6e350d71957764998b645208d0c51c
Angel Monroy <angelmc@outlook.com>
2025-06-30 14:47:27 -0600

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,42 @@
-<><script>
-    window.addEventListener('load', function() {setTimeout(function () {
-        var tawk = document.createElement('script');
-        tawk.async = true;
-        tawk.src = 'https://embed.tawk.to/6856183dc44c79190f98815b/1iu84tcbu';
-        document.body.appendChild(tawk);
-    }, 5000)};
-    });
-</script><script>
-        const isSlowDevice = /(Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|Tablet)/i.test(navigator.userAgent)
-        && !/Chrome|Safari|Firefox|Edge|SamsungBrowser|Opera/i.test(navigator.userAgent);
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6856183dc44c79190f98815b/1iu84tcbu';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
 
-        if (isSlowDevice) {document.documentElement.style.setProperty('--animation-duration', '0.5s')};
-        const preloads = [
-        '/assets/img/hero-bg.webp',
-        '/assets/css/style.css'
-        ];
-        preloads.forEach(resource => { }
-        const link = document.createElement('link');
-        link.rel = 'preload';
-        link.href = resource;
-        link.as = resource.includes('.css') ? 'style' : 'image';
-        document.head.appendChild(link);
-        });
-        }
-    </script><script>
-        if('serviceWorker' in navigator) {window.addEventListener('load', () => {
-            navigator.serviceWorker.register('assets/js/sw.js');
-        })};
-        }
-    </script>
-    </>
+<script>
+// Device detection
+const isSlowDevice = /(Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|Tablet)/i.test(navigator.userAgent)
+    && !/Chrome|Safari|Firefox|Edge|SamsungBrowser|Opera/i.test(navigator.userAgent);
+
+if (isSlowDevice) {
+    document.documentElement.style.setProperty('--animation-duration', '0.5s');
+}
+
+// Resource preloading
+const preloads = [
+    '/assets/img/hero-bg.webp',
+    '/assets/css/style.css'
+];
+
+preloads.forEach(resource => {
+    const link = document.createElement('link');
+    link.rel = 'preload';
+    link.href = resource;
+    link.as = resource.includes('.css') ? 'style' : 'image';
+    document.head.appendChild(link);
+});
+
+// Service Worker registration
+if('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('assets/js/sw.js');
+    });
+}
+</script>
